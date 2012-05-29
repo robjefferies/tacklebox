0980b9c057543642c0ea45dc335ccbb442a55712
Rob Jefferies <rob.jefferies@gmail.com>
2012-05-29 17:29:08 -0500

    Initial Commit

---
+++ Tacklebox/Configuration/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+using Tacklebox.Extensions;
+
+namespace Tacklebox.Configuration
+{
+    public class Config
+    {
+        public static string Get(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        public static int GetInt32(string key)
+        {
+            return Get(key).ToInt32();
+        }
+
+        public static DateTime GetDateTime(string key)
+        {
+            return Get(key).ToDateTime();
+        }
+
+    }
+}
---
+++ Tacklebox/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tacklebox.Extensions
+{
+    public static class StringExtensions
+    {
+        public static int ToInt32(this string self)
+        {
+            return Int32.Parse(self);
+        }
+
+        public static DateTime ToDateTime(this string self)
+        {
+            return DateTime.Parse(self);
+        }
+    }
+}
