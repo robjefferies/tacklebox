0877578082e47f189342a1b30c854833d81de069
Rob Jefferies <rob.jefferies@gmail.com>
2012-06-01 14:07:37 -0500

    Added age functions

---
+++ Tacklebox.Tests/Helper/DateHelperTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Tacklebox.Helper;
+
+namespace Tacklebox.Tests.Helper
+{
+    [TestFixture]
+    public class DateHelperTest
+    {
+        [Test]
+        public void AgeTest()
+        {
+            DateTime birthday = new DateTime(1980, 1, 1);
+
+            Assert.AreEqual(0,DateHelper.Age(birthday,birthday)); // Same Day
+            Assert.AreEqual(1, DateHelper.Age(birthday, birthday.AddYears(1))); // One Year
+            Assert.AreEqual(0,DateHelper.Age(birthday,birthday.AddYears(1).AddDays(-1))); // One day less than a year
+            Assert.AreEqual(10,DateHelper.Age(birthday,birthday.AddYears(10).AddMonths(1).AddDays(3))); // 10 year, 1 month , 3 dys
+
+        }
+    }
+}
---
+++ Tacklebox/Helper/DateHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Tacklebox.Extensions;
+
+namespace Tacklebox.Helper
+{
+    public class DateHelper
+    {
+        /* Determines the number of years between two dates.  What's needed for Age or Years of Safety etc. */
+        public static int Age(DateTime date1, DateTime date2)
+        {
+            DateTime startDate = date1.Min(date2);
+            DateTime endDate = date1.Max(date2);
+
+            int age = endDate.Year - startDate.Year;
+            if (startDate > endDate.AddYears(-age)) age--;
+            return age;
+        } 
+    }
+}
