81ecf28a66684b08a6959b05c8c29f1df50dd885
Rob Jefferies <robjefferies@mundycos.com>
2013-06-03 17:17:32 -0500

    Silent File Delete Function

---
+++ Tacklebox.Tests/IO/FileTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tacklebox.Tests.IO
+{
+    [TestFixture]
+    class FileTests
+    {
+        [Test]
+        public void TestDeleteSilent()
+        {
+            const string FILENAME = "test-file.txt";
+
+            var lines = new List<string>() { "One","Two","three"};
+            System.IO.File.WriteAllLines(FILENAME, lines);
+
+            // Test delete when the file should exist
+            Assert.AreEqual(true, System.IO.File.Exists(FILENAME));
+            Tacklebox.IO.File.DeleteSilent(FILENAME);
+            Assert.AreEqual(false, System.IO.File.Exists(FILENAME));
+
+            // Test delete when the file should not exist
+            Assert.AreEqual(false, System.IO.File.Exists(FILENAME));
+            Tacklebox.IO.File.DeleteSilent(FILENAME);
+            Assert.AreEqual(false, System.IO.File.Exists(FILENAME));
+
+        }
+    }
+}
---
+++ Tacklebox/IO/File.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tacklebox.IO
+{
+    public class File
+    {
+        public static void DeleteSilent(string filename)
+        {
+            try
+            {
+                System.IO.File.Delete(filename);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
