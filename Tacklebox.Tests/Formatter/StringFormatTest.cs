600e8849e867c5ee81f50889bb863eb519c065c8
Rob Jefferies <robjefferies@mundycos.com>
2013-06-20 11:59:58 -0500

    Added DigitsOnly formatter for strings

--- Tacklebox.Tests/Formatter/StringFormatTest.cs
+++ Tacklebox.Tests/Formatter/StringFormatTest.cs
@@ -14,5 +14,13 @@ namespace Tacklebox.Tests.Formatter
             Assert.AreEqual("000-45-6789", StringFormat.SSN("456789"));
             Assert.AreEqual("000-45-6789", StringFormat.SSN(456789));
         }
+
+        [Test]
+        public void TestDigitsOnly()
+        {
+            Assert.AreEqual("2819836078",StringFormat.DigitsOnly("(281) 983-6078"));
+            Assert.AreEqual("", StringFormat.DigitsOnly("hello world"));
+            Assert.AreEqual("1000000", StringFormat.DigitsOnly("-10,000.00"));
+        }
     }
 }
--- Tacklebox/Formatter/StringFormat.cs
+++ Tacklebox/Formatter/StringFormat.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Tacklebox.Extensions;
 
 namespace Tacklebox.Formatter
@@ -13,6 +14,12 @@ namespace Tacklebox.Formatter
         public static string SSN(int ssn)
         {
             return String.Format("{0:000-00-0000}", ssn);
+        }
+
+        // http://stackoverflow.com/a/3532948/254058
+        public static string DigitsOnly(string s)
+        {
+            return new string(s.Where(c => char.IsDigit(c)).ToArray());
         }
     }
 }
